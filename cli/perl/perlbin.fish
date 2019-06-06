45094df034260c6e12843fb29ba8e95e675e3b18
sipo <sipomatadorducosmos@gmail.com>
2019-06-06 22:23:40 +0200

    Suppr Perl

--- cli/perl/perlbin.fish
+++
@@ -1,7 +0,0 @@
-if status --is-login
-    for perldir in /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
-        if test -d $perldir; and not contains $perldir $PATH
-            set PATH $PATH $perldir
-        end
-    end
-end
